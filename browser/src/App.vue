009275f7657a3a5c8aa1e60aa32c77f8529f5b94
yxie@nbai.io <NEU@user123>
2021-09-16 23:25:19 +0800

    backup to filcoin-add tabs

--- browser/src/App.vue
+++ browser/src/App.vue
@@ -1,10 +1,29 @@
 <template>
     <div id="app">
-        <router-view></router-view>
+        <router-view v-if="isRouterAlive"></router-view>
     </div>
 </template>
 <script>
 export default {
+  name: 'APP',
+  provide () {
+      return {
+          reload: this.reload
+      }
+  },
+  data() {
+      return{
+          isRouterAlive: true
+      }
+  },
+  methods: {
+      reload () {
+          this.isRouterAlive = false;
+          this.$nextTick(function () {
+              this.isRouterAlive = true;
+          })
+      }
+  },
   mounted() {
     /* window.onbeforeunload = function(){
       sessionStorage.removeItem('addrWeb')
